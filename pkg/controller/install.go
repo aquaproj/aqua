13e6c495fd0773eab769d06efb64e6d6bb6c19d5
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-08-28 00:55:59 +0900

    feat: check file.src after the package is installed

--- pkg/controller/install.go
+++ pkg/controller/install.go
@@ -153,6 +153,40 @@ func (ctrl *Controller) installPackage(ctx context.Context, inlineRepo map[strin
 		}
 	}
 
+	for _, file := range pkgInfo.Files {
+		if file.Src.Empty() {
+			continue
+		}
+		logE := logrus.WithFields(logrus.Fields{
+			"file_name": file.Name,
+		})
+		fileSrc, err := file.Src.Execute(map[string]interface{}{
+			"Package":     pkg,
+			"PackageInfo": pkgInfo,
+			"OS":          runtime.GOOS,
+			"Arch":        runtime.GOARCH,
+			"File":        file,
+		})
+		if err != nil {
+			logE.WithError(err).Warn("render the file.src")
+			continue
+		}
+		exePath := filepath.Join(pkgPath, fileSrc)
+		logE = logE.WithFields(logrus.Fields{
+			"exe_path": exePath,
+			"file.src": fileSrc,
+		})
+		finfo, err := os.Stat(exePath)
+		if err != nil {
+			logE.WithError(err).Warn("exe_path isn't found. Check file.src is correct")
+			continue
+		}
+		if finfo.IsDir() {
+			logE.Warn("exe_path is directory. Check file.src is correct")
+			continue
+		}
+	}
+
 	return nil
 }
 
