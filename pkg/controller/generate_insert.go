098292791f921d8b0a2023d2cbf5a4044b5402a0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-02-23 16:27:47 +0900

    refactor: refactor updating YAML

--- pkg/controller/generate_insert.go
+++ pkg/controller/generate_insert.go
@@ -5,11 +5,10 @@ import (
 	"os"
 
 	"github.com/goccy/go-yaml"
-	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
 )
 
-func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) error { //nolint:cyclop
+func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) error {
 	file, err := parser.ParseFile(cfgFilePath, parser.ParseComments)
 	if err != nil {
 		return fmt.Errorf("parse configuration file as YAML: %w", err)
@@ -18,37 +17,19 @@ func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) err
 	if err != nil {
 		return fmt.Errorf("convert packages to node: %w", err)
 	}
+	path, err := yaml.PathString("$.packages")
+	if err != nil {
+		return fmt.Errorf("build a YAML Path: %w", err)
+	}
+	if err := path.MergeFromNode(file, node); err != nil {
+		return fmt.Errorf("add packages to AST: %w", err)
+	}
 
-	changed := false
-	for _, docNode := range file.Docs {
-		body := docNode.Body
-		mapping, ok := body.(*ast.MappingNode)
-		if !ok {
-			continue
-		}
-		for _, mv := range mapping.Values {
-			if mv.Key.String() != "packages" {
-				continue
-			}
-			seq, ok := mv.Value.(*ast.SequenceNode)
-			if !ok {
-				continue
-			}
-			if err := ast.Merge(seq, node); err != nil {
-				return fmt.Errorf("merge node: %w", err)
-			}
-			changed = true
-		}
-		break
-	}
-	if !changed {
-		return nil
-	}
 	stat, err := os.Stat(cfgFilePath)
 	if err != nil {
 		return fmt.Errorf("get configuration file stat: %w", err)
 	}
-	if err := os.WriteFile(cfgFilePath, []byte(file.String()), stat.Mode()); err != nil {
+	if err := os.WriteFile(cfgFilePath, []byte(file.String()+"\n"), stat.Mode()); err != nil {
 		return fmt.Errorf("write the configuration file: %w", err)
 	}
 	return nil
