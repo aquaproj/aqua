6304e550141f2f9aac86df6b08c9a39b3cb18ad3
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-02-24 09:26:19 +0900

    fix: stop using YAML Path because YAML Path breaks indent

--- pkg/controller/generate_insert.go
+++ pkg/controller/generate_insert.go
@@ -8,7 +8,6 @@ import (
 	"github.com/goccy/go-yaml"
 	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
-	"github.com/goccy/go-yaml/printer"
 )
 
 func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) error {
@@ -25,8 +24,7 @@ func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) err
 	if err != nil {
 		return fmt.Errorf("get configuration file stat: %w", err)
 	}
-	var p printer.Printer
-	if err := os.WriteFile(cfgFilePath, p.PrintNode(file.Docs[0]), stat.Mode()); err != nil {
+	if err := os.WriteFile(cfgFilePath, []byte(file.String()+"\n"), stat.Mode()); err != nil {
 		return fmt.Errorf("write the configuration file: %w", err)
 	}
 	return nil
@@ -37,29 +35,28 @@ func (ctrl *Controller) updateASTFile(file *ast.File, pkgs interface{}) error {
 	if err != nil {
 		return fmt.Errorf("convert packages to node: %w", err)
 	}
-	path, err := yaml.PathString("$.packages")
-	if err != nil {
-		return fmt.Errorf("build a YAML Path: %w", err)
-	}
-	base, err := path.FilterFile(file)
-	if err != nil {
-		if errors.Is(err, yaml.ErrNotFoundNode) {
-			if err := path.ReplaceWithNode(file, node); err != nil {
-				return fmt.Errorf("replace node: %w", err)
+
+	for _, doc := range file.Docs {
+		body, ok := doc.Body.(*ast.MappingNode)
+		if !ok {
+			continue
+		}
+		for _, mapValue := range body.Values {
+			if mapValue.Key.String() != "packages" {
+				continue
 			}
-			return nil
+			switch mapValue.Value.Type() {
+			case ast.NullType:
+				mapValue.Value = node
+			case ast.SequenceType:
+				if err := ast.Merge(mapValue.Value, node); err != nil {
+					return fmt.Errorf("merge packages: %w", err)
+				}
+			default:
+				return errors.New("packages must be null or array")
+			}
+			break
 		}
-		return fmt.Errorf("get packages with YAML Path: %w", err)
-	}
-	if base.Type() == ast.NullType {
-		ctrl.logE().Info("replace null node")
-		if err := path.ReplaceWithNode(file, node); err != nil {
-			return fmt.Errorf("replace node: %w", err)
-		}
-		return nil
-	}
-	if err := ast.Merge(base, node); err != nil {
-		return fmt.Errorf("add packages to AST: %w", err)
 	}
 	return nil
 }
