2f613ec7947c5dc2cd79907972852093743b890e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-02-24 09:11:19 +0900

    fix: add packages properly even if `packages:` is empty

--- pkg/controller/generate_insert.go
+++ pkg/controller/generate_insert.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/goccy/go-yaml"
+	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
+	"github.com/goccy/go-yaml/printer"
 )
 
 func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) error {
@@ -13,6 +16,23 @@ func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) err
 	if err != nil {
 		return fmt.Errorf("parse configuration file as YAML: %w", err)
 	}
+
+	if err := ctrl.updateASTFile(file, pkgs); err != nil {
+		return err
+	}
+
+	stat, err := os.Stat(cfgFilePath)
+	if err != nil {
+		return fmt.Errorf("get configuration file stat: %w", err)
+	}
+	var p printer.Printer
+	if err := os.WriteFile(cfgFilePath, p.PrintNode(file.Docs[0]), stat.Mode()); err != nil {
+		return fmt.Errorf("write the configuration file: %w", err)
+	}
+	return nil
+}
+
+func (ctrl *Controller) updateASTFile(file *ast.File, pkgs interface{}) error {
 	node, err := yaml.ValueToNode(pkgs)
 	if err != nil {
 		return fmt.Errorf("convert packages to node: %w", err)
@@ -21,16 +41,25 @@ func (ctrl *Controller) generateInsert(cfgFilePath string, pkgs interface{}) err
 	if err != nil {
 		return fmt.Errorf("build a YAML Path: %w", err)
 	}
-	if err := path.MergeFromNode(file, node); err != nil {
-		return fmt.Errorf("add packages to AST: %w", err)
-	}
-
-	stat, err := os.Stat(cfgFilePath)
+	base, err := path.FilterFile(file)
 	if err != nil {
-		return fmt.Errorf("get configuration file stat: %w", err)
+		if errors.Is(err, yaml.ErrNotFoundNode) {
+			if err := path.ReplaceWithNode(file, node); err != nil {
+				return fmt.Errorf("replace node: %w", err)
+			}
+			return nil
+		}
+		return fmt.Errorf("get packages with YAML Path: %w", err)
 	}
-	if err := os.WriteFile(cfgFilePath, []byte(file.String()+"\n"), stat.Mode()); err != nil {
-		return fmt.Errorf("write the configuration file: %w", err)
+	if base.Type() == ast.NullType {
+		ctrl.logE().Info("replace null node")
+		if err := path.ReplaceWithNode(file, node); err != nil {
+			return fmt.Errorf("replace node: %w", err)
+		}
+		return nil
+	}
+	if err := ast.Merge(base, node); err != nil {
+		return fmt.Errorf("add packages to AST: %w", err)
 	}
 	return nil
 }
