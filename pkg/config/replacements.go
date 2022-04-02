db266c6a87c754c89e57f93dcc1a1d46b73a3c55
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-04-02 17:33:49 +0900

    feat: add enum to JSON Schema of GOOS and GOARCH

--- pkg/config/replacements.go
+++ pkg/config/replacements.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Override struct {
-	GOOS         string             `json:"goos,omitempty" jsonschema:"example=darwin,example=linux"`
-	GOArch       string             `json:"goarch,omitempty" jsonschema:"example=amd64,example=arm64"`
+	GOOS         string             `json:"goos,omitempty" jsonschema:"enum=aix,enum=android,enum=darwin,enum=dragonfly,enum=freebsd,enum=illumos,enum=ios,enum=js,enum=linux,enum=netbsd,enum=openbsd,enum=plan9,enum=solaris,enum=windows"`
+	GOArch       string             `json:"goarch,omitempty" jsonschema:"enum=386,enum=amd64,enum=arm,enum=arm64,enum=mips,enum=mips64,enum=mips64le,enum=mipsle,enum=ppc64,enum=ppc64le,enum=riscv64,enum=s390x,enum=wasm"`
 	Replacements map[string]string  `json:"replacements,omitempty"`
 	Format       string             `json:"format,omitempty" jsonschema:"example=tar.gz,example=raw"`
 	Asset        *template.Template `json:"asset,omitempty"`
