9cc82b5229d4ed446a26c6ae3145bca355b07683
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-13 21:59:16 +0900

    test: add test of exec

---
+++ pkg/exec/exec_test.go
@@ -0,0 +1,45 @@
+package exec_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aquaproj/aqua/pkg/exec"
+)
+
+func TestExecutorExec(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		name     string
+		exePath  string
+		args     []string
+		isErr    bool
+		exitCode int
+	}{
+		{
+			name:    "/bin/date",
+			exePath: "/bin/date",
+		},
+	}
+	executor := exec.New()
+	ctx := context.Background()
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			t.Parallel()
+			exitCode, err := executor.Exec(ctx, d.exePath, d.args)
+			if d.isErr {
+				if err == nil {
+					t.Fatal("err should be returned")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+			if d.exitCode != exitCode {
+				t.Fatalf("wanted %v, got %v", d.exitCode, exitCode)
+			}
+		})
+	}
+}
