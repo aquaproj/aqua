d283fb5a262e7a4a4549aa57568d4f0992ff31ab
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-06-10 12:39:33 +0900

    fix: remove registry validation

--- pkg/validate/error.go
+++ pkg/validate/error.go
@@ -2,7 +2,4 @@ package validate
 
 import "errors"
 
-var (
-	errPkgNameMustBeUniqueInRegistry = errors.New("the package name must be unique in the same registry")
-	ErrRegistryNameIsDuplicated      = errors.New("registry name is duplicated")
-)
+var ErrRegistryNameIsDuplicated = errors.New("registry name is duplicated")
