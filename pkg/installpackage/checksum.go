e66df384750a47eccb58910d44d14d11c34e08ca
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-07-27 19:11:04 +0900

    refactor: refactor

--- pkg/installpackage/checksum.go
+++ pkg/installpackage/checksum.go
@@ -1,6 +1,16 @@
 package installpackage
 
-import "github.com/spf13/afero"
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+
+	"github.com/aquaproj/aqua/pkg/config"
+	"github.com/codingsince1985/checksum"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+	"github.com/suzuki-shunsuke/logrus-error/logerr"
+)
 
 func (inst *installer) ReadChecksumFile(fs afero.Fs, p string) error {
 	return inst.checksums.ReadFile(fs, p) //nolint:wrapcheck
@@ -8,4 +18,54 @@ func (inst *installer) ReadChecksumFile(fs afero.Fs, p string) error {
 
 func (inst *installer) UpdateChecksumFile(fs afero.Fs, p string) error {
 	return inst.checksums.UpdateFile(fs, p) //nolint:wrapcheck
+}
+
+func (inst *installer) verifyChecksum(pkg *config.Package, assetName string, body io.Reader) (io.ReadCloser, error) { //nolint:cyclop
+	pkgInfo := pkg.PackageInfo
+	tempDir, err := afero.TempDir(inst.fs, "", "")
+	if err != nil {
+		return nil, fmt.Errorf("create a temporal directory: %w", err)
+	}
+	defer inst.fs.RemoveAll(tempDir) //nolint:errcheck
+	tempFilePath := filepath.Join(tempDir, assetName)
+	if assetName == "" && (pkgInfo.Type == "github_archive" || pkgInfo.Type == "go") {
+		tempFilePath = filepath.Join(tempDir, "archive.tar.gz")
+	}
+	file, err := inst.fs.Create(tempFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("create a temporal file: %w", logerr.WithFields(err, logrus.Fields{
+			"temp_file": tempFilePath,
+		}))
+	}
+	defer file.Close()
+	if _, err := io.Copy(file, body); err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+	sha256, err := checksum.SHA256sum(tempFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("calculate a checksum of downloaded file: %w", logerr.WithFields(err, logrus.Fields{
+			"temp_file": tempFilePath,
+		}))
+	}
+
+	checksumID, err := pkg.GetChecksumID(inst.runtime)
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+	chksum := inst.checksums.Get(checksumID)
+
+	if chksum != "" && sha256 != chksum {
+		return nil, logerr.WithFields(errInvalidChecksum, logrus.Fields{ //nolint:wrapcheck
+			"actual_checksum":   sha256,
+			"expected_checksum": chksum,
+		})
+	}
+	if chksum == "" {
+		inst.checksums.Set(checksumID, sha256)
+	}
+	readFile, err := inst.fs.Open(tempFilePath)
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+	return readFile, nil
 }
--- pkg/installpackage/download.go
+++ pkg/installpackage/download.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"path/filepath"
 
 	"github.com/aquaproj/aqua/pkg/config"
 	"github.com/aquaproj/aqua/pkg/unarchive"
-	"github.com/codingsince1985/checksum"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/afero"
-	"github.com/suzuki-shunsuke/logrus-error/logerr"
 )
 
-func (inst *installer) download(ctx context.Context, pkg *config.Package, dest, assetName string, checksumEnabled bool, logE *logrus.Entry) error { //nolint:funlen,gocognit,cyclop
+func (inst *installer) download(ctx context.Context, pkg *config.Package, dest, assetName string, checksumEnabled bool, logE *logrus.Entry) error {
 	logE = logE.WithFields(logrus.Fields{
 		"package_name":    pkg.Package.Name,
 		"package_version": pkg.Package.Version,
@@ -38,51 +34,10 @@ func (inst *installer) download(ctx context.Context, pkg *config.Package, dest, 
 
 	var readBody io.Reader = body
 
-	if checksumEnabled { //nolint:nestif
-		tempDir, err := afero.TempDir(inst.fs, "", "")
+	if checksumEnabled {
+		readFile, err := inst.verifyChecksum(pkg, assetName, body)
 		if err != nil {
-			return fmt.Errorf("create a temporal directory: %w", err)
-		}
-		defer inst.fs.RemoveAll(tempDir) //nolint:errcheck
-		tempFilePath := filepath.Join(tempDir, assetName)
-		if assetName == "" && (pkgInfo.Type == "github_archive" || pkgInfo.Type == "go") {
-			tempFilePath = filepath.Join(tempDir, "archive.tar.gz")
-		}
-		file, err := inst.fs.Create(tempFilePath)
-		if err != nil {
-			return fmt.Errorf("create a temporal file: %w", logerr.WithFields(err, logrus.Fields{
-				"temp_file": tempFilePath,
-			}))
-		}
-		defer file.Close()
-		if _, err := io.Copy(file, body); err != nil {
-			return err //nolint:wrapcheck
-		}
-		sha256, err := checksum.SHA256sum(tempFilePath)
-		if err != nil {
-			return fmt.Errorf("calculate a checksum of downloaded file: %w", logerr.WithFields(err, logrus.Fields{
-				"temp_file": tempFilePath,
-			}))
-		}
-
-		checksumID, err := pkg.GetChecksumID(inst.runtime)
-		if err != nil {
-			return err //nolint:wrapcheck
-		}
-		chksum := inst.checksums.Get(checksumID)
-
-		if chksum != "" && sha256 != chksum {
-			return logerr.WithFields(errInvalidChecksum, logrus.Fields{ //nolint:wrapcheck
-				"actual_checksum":   sha256,
-				"expected_checksum": chksum,
-			})
-		}
-		if chksum == "" {
-			inst.checksums.Set(checksumID, sha256)
-		}
-		readFile, err := inst.fs.Open(tempFilePath)
-		if err != nil {
-			return err //nolint:wrapcheck
+			return err
 		}
 		if readFile != nil {
 			defer readFile.Close()
