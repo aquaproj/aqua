0ae88272dfc706a9cccc9f24fb0435713dd208fa
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-08-01 22:03:24 +0900

    fix: add "algorithm"

--- pkg/config/registry/checksum.go
+++ pkg/config/registry/checksum.go
@@ -4,6 +4,7 @@ type Checksum struct {
 	Type       string           `json:"type"`
 	Path       string           `json:"path,omitempty"`
 	FileFormat string           `yaml:"file_format" json:"file_format"`
+	Algorithm  string           `json:"algorithm,omitempty"`
 	Pattern    *ChecksumPattern `json:"pattern,omitempty"`
 }
 
