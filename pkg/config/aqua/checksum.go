602c7c38e087ccf34fb0c2c6be7460dfd1980732
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-08-08 09:20:25 +0900

    feat: add checksum config

--- pkg/config/aqua/checksum.go
+++ pkg/config/aqua/checksum.go
@@ -5,11 +5,26 @@ func (cfg *Config) ChecksumEnabled() bool {
 		return false
 	}
 	if cfg.Checksum == nil {
-		return false
+		return true
 	}
-	return cfg.Checksum.Enabled
+	if cfg.Checksum.Enabled == nil {
+		return true
+	}
+	return *cfg.Checksum.Enabled
 }
 
 type Checksum struct {
-	Enabled bool `json:"enabled"`
+	Enabled                  *bool              `json:"enabled,omitempty"`
+	RequireChecksumInAdvance bool               `yaml:"require_checksum_in_advance" json:"require_checksum_in_advance,omitempty"`
+	CreateJSON               bool               `yaml:"create_json" json:"create_json,omitempty"`
+	RequireChecksum          bool               `yaml:"require_checksum" json:"require_checksum,omitempty"`
+	SaveCalculatedChecksum   bool               `yaml:"save_calculated_checksum" json:"save_calculated_checksum,omitempty"`
+	Excludes                 []*ChekcsumExclude `json:"excludes,omitempty"`
+}
+
+type ChekcsumExclude struct {
+	Name     string   `json:"name,omitempty"`
+	Registry string   `json:"registry,omitempty"`
+	Version  string   `json:"version,omitempty"`
+	Envs     []string `json:"envs,omitempty"`
 }
