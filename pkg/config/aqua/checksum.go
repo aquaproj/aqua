65cc089b8b03c2a07a63a3c035c8ab345e50bb94
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-10-02 15:28:19 +0900

    fix: comment out unimplemented feature configuration

--- pkg/config/aqua/checksum.go
+++ pkg/config/aqua/checksum.go
@@ -8,17 +8,17 @@ func (cfg *Config) ChecksumEnabled() bool {
 }
 
 type Checksum struct {
-	Enabled                  *bool              `json:"enabled,omitempty"`
-	RequireChecksumInAdvance bool               `yaml:"require_checksum_in_advance" json:"-"`
-	CreateJSON               bool               `yaml:"create_json" json:"-"`
-	RequireChecksum          bool               `yaml:"require_checksum" json:"-"`
-	SaveCalculatedChecksum   bool               `yaml:"save_calculated_checksum" json:"-"`
-	Excludes                 []*ChekcsumExclude `json:"-"`
+	Enabled *bool `json:"enabled,omitempty"`
+	// RequireChecksumInAdvance bool               `yaml:"require_checksum_in_advance" json:"-"`
+	// CreateJSON               bool               `yaml:"create_json" json:"-"`
+	RequireChecksum bool `yaml:"require_checksum" json:"-"`
+	// SaveCalculatedChecksum   bool               `yaml:"save_calculated_checksum" json:"-"`
+	// Excludes                 []*ChekcsumExclude `json:"-"`
 }
 
-type ChekcsumExclude struct {
-	Name     string   `json:"name,omitempty"`
-	Registry string   `json:"registry,omitempty"`
-	Version  string   `json:"version,omitempty"`
-	Envs     []string `json:"envs,omitempty"`
-}
+// type ChekcsumExclude struct {
+// 	Name     string   `json:"name,omitempty"`
+// 	Registry string   `json:"registry,omitempty"`
+// 	Version  string   `json:"version,omitempty"`
+// 	Envs     []string `json:"envs,omitempty"`
+// }
