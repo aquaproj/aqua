4f8389cad8af257156bdd05e885399ab49641e7d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-10-09 18:56:24 +0900

    fix(jsonschema): update jsonschema

--- pkg/config/aqua/checksum.go
+++ pkg/config/aqua/checksum.go
@@ -18,7 +18,7 @@ type Checksum struct {
 	Enabled *bool `json:"enabled,omitempty"`
 	// RequireChecksumInAdvance bool               `yaml:"require_checksum_in_advance" json:"-"`
 	// CreateJSON               bool               `yaml:"create_json" json:"-"`
-	RequireChecksum bool `yaml:"require_checksum" json:"require_checksum"`
+	RequireChecksum bool `yaml:"require_checksum" json:"require_checksum,omitempty"`
 	// SaveCalculatedChecksum   bool               `yaml:"save_calculated_checksum" json:"-"`
 	// Excludes                 []*ChekcsumExclude `json:"-"`
 }
