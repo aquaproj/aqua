fc1b73ad2bb551871167d313a2140e44167b33d2
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-10-29 08:36:21 +0900

    refactor: fix cyclop

--- pkg/config/aqua/tag.go
+++ pkg/config/aqua/tag.go
@@ -5,21 +5,17 @@ func FilterPackageByTag(pkg *Package, tags, excludedTags map[string]struct{}) bo
 		return len(tags) == 0
 	}
 	if len(excludedTags) == 0 {
-		if len(tags) == 0 {
-			return true
-		}
-		for _, tag := range pkg.Tags {
-			if _, ok := tags[tag]; ok {
-				return true
-			}
-		}
-		return false
+		return matchTags(pkg, tags)
 	}
 	for _, tag := range pkg.Tags {
 		if _, ok := excludedTags[tag]; ok {
 			return false
 		}
 	}
+	return matchTags(pkg, tags)
+}
+
+func matchTags(pkg *Package, tags map[string]struct{}) bool {
 	if len(tags) == 0 {
 		return true
 	}
