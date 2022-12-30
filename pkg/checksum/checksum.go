8a1c97633d43eb8c92df27dba1a3d780e5196920
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-12-31 08:47:52 +0900

    refactor(checksum): merge function

--- pkg/checksum/checksum.go
+++ pkg/checksum/checksum.go
@@ -21,20 +21,12 @@ func NewCalculator() *Calculator {
 type Calculator struct{}
 
 func (calc *Calculator) Calculate(fs afero.Fs, filename, algorithm string) (string, error) {
-	switch algorithm {
-	case "md5":
-		return checksum.MD5sum(filename) //nolint:wrapcheck
-	case "sha256":
-		return checksum.SHA256sum(filename) //nolint:wrapcheck
-	case "sha512":
-		return calculateSHA512(fs, filename)
-	case "sha1":
-		return calculateSHA1(fs, filename)
-	case "":
-		return "", errors.New("algorithm is required")
-	default:
-		return "", errors.New("unsupported algorithm")
+	f, err := fs.Open(filename)
+	if err != nil {
+		return "", fmt.Errorf("open a file to calculate the checksum: %w", err)
 	}
+	defer f.Close()
+	return CalculateReader(f, algorithm)
 }
 
 func CalculateReader(file io.Reader, algorithm string) (string, error) {
@@ -65,24 +57,6 @@ func calculateSHA1Reader(file io.Reader) (string, error) {
 
 func calculateSHA512Reader(file io.Reader) (string, error) {
 	byt, err := io.ReadAll(file)
-	if err != nil {
-		return "", fmt.Errorf("read a file: %w", err)
-	}
-	sum := sha512.Sum512(byt)
-	return hex.EncodeToString(sum[:]), nil
-}
-
-func calculateSHA1(fs afero.Fs, filename string) (string, error) {
-	byt, err := afero.ReadFile(fs, filename)
-	if err != nil {
-		return "", fmt.Errorf("read a file: %w", err)
-	}
-	sum := sha1.Sum(byt) //nolint:gosec
-	return hex.EncodeToString(sum[:]), nil
-}
-
-func calculateSHA512(fs afero.Fs, filename string) (string, error) {
-	byt, err := afero.ReadFile(fs, filename)
 	if err != nil {
 		return "", fmt.Errorf("read a file: %w", err)
 	}
