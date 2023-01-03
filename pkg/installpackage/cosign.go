fab838921df71a345ddf43fa3f13018cf05e7747
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-01-03 09:06:49 +0900

    refactor: move cosign checksums to the package cosign

--- pkg/installpackage/cosign.go
+++ pkg/installpackage/cosign.go
@@ -67,14 +67,7 @@ func (cos *Cosign) InstallCosign(ctx context.Context, logE *logrus.Entry, versio
 		},
 	}
 
-	checksums := map[string]string{
-		"darwin/amd64":  "1d164b8b1fcfef1e1870d809edbb9862afd5995cab63687a440b84cca5680ecf",
-		"darwin/arm64":  "02bef878916be048fd7dcf742105639f53706a59b5b03f4e4eaccc01d05bc7ab",
-		"linux/amd64":   "a50651a67b42714d6f1a66eb6773bf214dacae321f04323c0885f6a433051f95",
-		"linux/arm64":   "a7a79a52c7747e2c21554cad4600e6c7130c0429017dd258f9c558d957fa9090",
-		"windows/amd64": "78a2774b68b995cc698944f6c235b1c93dcb6d57593a58a565ee7a56d64e4b85",
-	}
-	chksum := checksums[cos.installer.runtime.Env()]
+	chksum := cosign.Checksums()[cos.installer.runtime.Env()]
 
 	pkgInfo, err := pkg.PackageInfo.Override(pkg.Package.Version, cos.installer.runtime)
 	if err != nil {
