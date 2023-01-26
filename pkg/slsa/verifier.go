16792c6a865b087001ba1350ad0ee48de99063f4
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-01-26 22:26:05 +0900

    fix: retry slsa-verifier when slsa-verifier failed

--- pkg/slsa/verifier.go
+++ pkg/slsa/verifier.go
@@ -2,13 +2,16 @@ package slsa
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/aquaproj/aqua/pkg/config/registry"
 	"github.com/aquaproj/aqua/pkg/download"
 	"github.com/aquaproj/aqua/pkg/runtime"
 	"github.com/aquaproj/aqua/pkg/template"
+	"github.com/aquaproj/aqua/pkg/util"
 	"github.com/sirupsen/logrus"
 	"github.com/slsa-framework/slsa-verifier/v2/cli/slsa-verifier/verify"
 	"github.com/spf13/afero"
@@ -55,25 +58,41 @@ func NewExecutor() *ExecutorImpl {
 }
 
 type Executor interface {
-	Verify(ctx context.Context, param *ParamVerify, provenancePath string) error
+	Verify(ctx context.Context, logE *logrus.Entry, param *ParamVerify, provenancePath string) error
 }
 
 type MockExecutor struct {
 	Err error
 }
 
-func (mock *MockExecutor) Verify(ctx context.Context, param *ParamVerify, provenancePath string) error {
+func (mock *MockExecutor) Verify(ctx context.Context, logE *logrus.Entry, param *ParamVerify, provenancePath string) error {
 	return mock.Err
 }
 
-func (exe *ExecutorImpl) Verify(ctx context.Context, param *ParamVerify, provenancePath string) error {
+func (exe *ExecutorImpl) Verify(ctx context.Context, logE *logrus.Entry, param *ParamVerify, provenancePath string) error {
 	v := verify.VerifyArtifactCommand{
 		ProvenancePath: provenancePath,
 		SourceURI:      param.SourceURI,
 		SourceTag:      &param.SourceTag,
 	}
-	if _, err := v.Exec(ctx, []string{param.ArtifactPath}); err != nil {
-		return fmt.Errorf("run slsa-verifier's verify-artifact command: %w", err)
+	for i := 0; i < 5; i++ {
+		_, err := v.Exec(ctx, []string{param.ArtifactPath})
+		if err == nil {
+			return nil
+		}
+		if e := ctx.Err(); e != nil {
+			return fmt.Errorf("run slsa-verifier's verify-artifact command: %w", err)
+		}
+		if !errors.Is(err, context.DeadlineExceeded) {
+			return fmt.Errorf("run slsa-verifier's verify-artifact command: %w", err)
+		}
+		if i == 4 { //nolint:gomnd
+			return fmt.Errorf("run slsa-verifier's verify-artifact command: %w", err)
+		}
+		logE.WithField("retry_count", i+1).Info("slsa-verifier failed. Retrying")
+		if err := util.Wait(ctx, 1*time.Second); err != nil {
+			return err //nolint:wrapcheck
+		}
 	}
 	return nil
 }
@@ -99,5 +118,5 @@ func (verifier *VerifierImpl) Verify(ctx context.Context, logE *logrus.Entry, rt
 		return fmt.Errorf("copy a provenance to a temporal file: %w", err)
 	}
 
-	return verifier.exe.Verify(ctx, param, provenanceFile.Name()) //nolint:wrapcheck
+	return verifier.exe.Verify(ctx, logE, param, provenanceFile.Name()) //nolint:wrapcheck
 }
