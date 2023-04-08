2fb145c727e786d9290895fd7b6c5e023d928704
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-04-08 14:37:52 +0900

    fix: replace ":" to "_" on Windows

--- pkg/policy/validate.go
+++ pkg/policy/validate.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/aquaproj/aqua/v2/pkg/config"
 	"github.com/aquaproj/aqua/v2/pkg/util"
@@ -58,8 +60,20 @@ var (
 	errPolicyUpdated  = errors.New("the policy file updated")
 )
 
+func normalizePath(p string) string {
+	// Replace ":" to "_" on Windows.
+	// https://www.ibm.com/docs/en/spectrum-archive-sde/2.4.1.0?topic=tips-file-name-characters
+	// On Windows systems, files and directory names cannot be created with a colon (:). But if a file or directory name is created with a colon on a Linux or Mac operating system, then moved to a Windows system, percent encoding is used to include the colon in the name in the index.
+	// Note: On a Windows system, the : (colon) character appears as “%3A” in the index with percent encoding. The colon appears as an “_” (underscore) in the file name on Windows Explorer, or in the command prompt.
+	if runtime.GOOS == "windows" {
+		return strings.ReplaceAll(p, ":", "_")
+	}
+	return p
+}
+
 func (validator *ValidatorImpl) Allow(p string) error {
-	policyPath := filepath.Join(validator.rootDir, "policies", p)
+	normalizedP := normalizePath(p)
+	policyPath := filepath.Join(validator.rootDir, "policies", normalizedP)
 	dir := filepath.Dir(policyPath)
 	fs := validator.fs
 	if err := util.MkdirAll(fs, dir); err != nil {
@@ -78,7 +92,7 @@ func (validator *ValidatorImpl) Allow(p string) error {
 	if _, err := io.Copy(f2, f1); err != nil {
 		return fmt.Errorf("copy a policy file: %w", err)
 	}
-	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", p)
+	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", normalizedP)
 	warnExist, err := afero.Exists(fs, warnFilePath)
 	if err != nil {
 		return fmt.Errorf("check if a warn file exists: %w", err)
@@ -93,7 +107,8 @@ func (validator *ValidatorImpl) Allow(p string) error {
 }
 
 func (validator *ValidatorImpl) Deny(p string) error {
-	policyPath := filepath.Join(validator.rootDir, "policies", p)
+	normalizedP := normalizePath(p)
+	policyPath := filepath.Join(validator.rootDir, "policies", normalizedP)
 	fs := validator.fs
 
 	// remove allow file
@@ -107,7 +122,7 @@ func (validator *ValidatorImpl) Deny(p string) error {
 		}
 	}
 
-	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", p)
+	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", normalizedP)
 	warnFileDir := filepath.Dir(warnFilePath)
 	if err := util.MkdirAll(fs, warnFileDir); err != nil {
 		return fmt.Errorf("create a directory where the policy warning file is stored: %w", err)
@@ -121,7 +136,7 @@ func (validator *ValidatorImpl) Deny(p string) error {
 }
 
 func (validator *ValidatorImpl) Warn(logE *logrus.Entry, policyFilePath string, updated bool) error {
-	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", policyFilePath)
+	warnFilePath := filepath.Join(validator.rootDir, "policy-warnings", normalizePath(policyFilePath))
 	fs := validator.fs
 	f, err := afero.Exists(fs, warnFilePath)
 	if err != nil {
@@ -153,7 +168,7 @@ func (validator *ValidatorImpl) Validate(p string) error {
 	if validator.disabled {
 		return nil
 	}
-	policyPath := filepath.Join(validator.rootDir, "policies", p)
+	policyPath := filepath.Join(validator.rootDir, "policies", normalizePath(p))
 	f, err := afero.Exists(validator.fs, policyPath)
 	if err != nil {
 		return fmt.Errorf("find a policy file: %w", err)
