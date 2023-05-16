67d7864f75a1a9d283d3447b1bababc47d063997
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-05-16 21:06:27 +0900

    feat: support pkg format on macOS

--- pkg/installpackage/public.go
+++ pkg/installpackage/public.go
@@ -7,5 +7,6 @@ import (
 type Executor interface {
 	HdiutilAttach(ctx context.Context, dmgPath, mountPoint string) (int, error)
 	HdiutilDetach(ctx context.Context, mountPath string) (int, error)
+	UnarchivePkg(ctx context.Context, pkgFilePath, dest string) (int, error)
 	ExecWithEnvs(ctx context.Context, exePath string, args, envs []string) (int, error)
 }
