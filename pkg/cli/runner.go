7dc5d9249f00be29b5296abeb1b11939e5c8fac8
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-05-21 15:16:15 +0900

    feat: add root-dir command (#2023)

--- pkg/cli/runner.go
+++ pkg/cli/runner.go
@@ -158,6 +158,7 @@ func (runner *Runner) Run(ctx context.Context, args ...string) error {
 			runner.newCompletionCommand(),
 			runner.newVersionCommand(),
 			runner.newCpCommand(),
+			runner.newRootDirCommand(),
 			runner.newUpdateChecksumCommand(),
 		},
 	}
