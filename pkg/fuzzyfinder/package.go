5607be469e984b4b9113c244e468f96394d8fa82
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-09-27 13:16:37 +0900

    style: format code

---
+++ pkg/fuzzyfinder/package.go
@@ -0,0 +1,55 @@
+package fuzzyfinder
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aquaproj/aqua/v2/pkg/config/registry"
+)
+
+type Package struct {
+	PackageInfo  *registry.PackageInfo
+	RegistryName string
+	Version      string
+}
+
+func (p *Package) Preview(w int) string {
+	return fmt.Sprintf("%s\n\n%s\n%s",
+		p.PackageInfo.GetName(),
+		p.PackageInfo.GetLink(),
+		formatDescription(p.PackageInfo.Description, w/2-8)) //nolint:gomnd
+}
+
+func (p *Package) Item() string {
+	files := p.PackageInfo.GetFiles()
+	fileNames := make([]string, 0, len(files))
+	for _, file := range files {
+		if file.Name == "" {
+			continue
+		}
+		fileNames = append(fileNames, file.Name)
+	}
+	fileNamesStr := strings.Join(fileNames, ", ")
+	aliases := make([]string, 0, len(p.PackageInfo.Aliases))
+	for _, alias := range p.PackageInfo.Aliases {
+		if alias.Name == "" {
+			continue
+		}
+		aliases = append(aliases, alias.Name)
+	}
+	pkgName := p.PackageInfo.GetName()
+	item := pkgName
+	if len(aliases) != 0 {
+		item += " (" + strings.Join(aliases, ", ") + ")"
+	}
+	if p.RegistryName != registryStandard {
+		item += " (" + p.RegistryName + ")"
+	}
+	if !strings.HasSuffix(pkgName, "/"+fileNamesStr) || pkgName == fileNamesStr {
+		item += " [" + fileNamesStr + "]"
+	}
+	if len(p.PackageInfo.SearchWords) > 0 {
+		item += ": " + strings.Join(p.PackageInfo.SearchWords, " ")
+	}
+	return item
+}
--- pkg/fuzzyfinder/package_test.go
+++ pkg/fuzzyfinder/package_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aquaproj/aqua/v2/pkg/config/registry"
 )
 
-func Test_find(t *testing.T) { //nolint:funlen
+func TestPackage_Item(t *testing.T) { //nolint:funlen
 	t.Parallel()
 	data := []struct {
 		name string
@@ -95,45 +95,7 @@ func Test_find(t *testing.T) { //nolint:funlen
 		d := d
 		t.Run(d.name, func(t *testing.T) {
 			t.Parallel()
-			s := find(d.pkg)
-			if s != d.exp {
-				t.Fatalf("wanted %s, got %s", d.exp, s)
-			}
-		})
-	}
-}
-
-func Test_getPreview(t *testing.T) {
-	t.Parallel()
-	data := []struct {
-		name string
-		pkg  *Package
-		i    int
-		w    int
-		exp  string
-	}{
-		{
-			name: "normal",
-			pkg: &Package{
-				PackageInfo: &registry.PackageInfo{
-					RepoOwner:   "suzuki-shunsuke",
-					RepoName:    "ci-info",
-					Description: "CLI tool to get CI related information",
-				},
-				RegistryName: registryStandard,
-			},
-			w: 100,
-			exp: `suzuki-shunsuke/ci-info
-
-https://github.com/suzuki-shunsuke/ci-info
-CLI tool to get CI related information`,
-		},
-	}
-	for _, d := range data {
-		d := d
-		t.Run(d.name, func(t *testing.T) {
-			t.Parallel()
-			s := getPreview(d.pkg, d.i, d.w)
+			s := d.pkg.Item()
 			if s != d.exp {
 				t.Fatalf("wanted %s, got %s", d.exp, s)
 			}
