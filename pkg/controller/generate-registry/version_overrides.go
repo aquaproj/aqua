be4494b215d2487c7acf88d5c20b9a442c5259e8
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-03-16 16:46:50 +0900

    fix: improve the logic to get the latest version (#2750)
    * fix: improve the logic to get the latest version
    
    * refactor: remove a duplicated code
    
    * fix: improve the logic to get the latest tag

--- pkg/controller/generate-registry/version_overrides.go
+++ pkg/controller/generate-registry/version_overrides.go
@@ -3,12 +3,12 @@ package genrgst
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"sort"
 
 	"github.com/aquaproj/aqua/v2/pkg/config/aqua"
 	"github.com/aquaproj/aqua/v2/pkg/config/registry"
 	"github.com/aquaproj/aqua/v2/pkg/github"
+	"github.com/aquaproj/aqua/v2/pkg/versiongetter"
 	"github.com/hashicorp/go-version"
 	"github.com/sirupsen/logrus"
 	"github.com/suzuki-shunsuke/logrus-error/logerr"
@@ -46,29 +46,12 @@ func listPkgsFromVersions(pkgName string, versions []string) []*aqua.Package {
 	return pkgs
 }
 
-var versionPattern = regexp.MustCompile(`^(.*?)v?((?:\d+)(?:\.\d+)?(?:\.\d+)?(?:(\.|-).+)?)$`)
-
-func GetVersionAndPrefix(tag string) (*version.Version, string, error) {
-	if v, err := version.NewVersion(tag); err == nil {
-		return v, "", nil
-	}
-	a := versionPattern.FindStringSubmatch(tag)
-	if a == nil {
-		return nil, "", nil
-	}
-	v, err := version.NewVersion(a[2])
-	if err != nil {
-		return nil, "", err //nolint:wrapcheck
-	}
-	return v, a[1], nil
-}
-
 func (c *Controller) getPackageInfoWithVersionOverrides(ctx context.Context, logE *logrus.Entry, pkgName string, pkgInfo *registry.PackageInfo, limit int) (*registry.PackageInfo, []string) {
 	ghReleases := c.listReleases(ctx, logE, pkgInfo, limit)
 	releases := make([]*Release, len(ghReleases))
 	for i, release := range ghReleases {
 		tag := release.GetTagName()
-		v, prefix, err := GetVersionAndPrefix(tag)
+		v, prefix, err := versiongetter.GetVersionAndPrefix(tag)
 		if err != nil {
 			logE.WithField("tag_name", tag).WithError(err).Warn("parse a tag as semver")
 		}
