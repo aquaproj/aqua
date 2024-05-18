5f9df452dabac6fe7d699eb3b9d496e8fb40c630
翠 / green <green@sapphi.red>
2024-05-18 19:39:17 +0900

    fix(info): mask username on Windows (#2902)
    * fix(info): mask username on Windows
    
    * chore: use `strings.Cut`
    
    Co-authored-by: Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>

--- pkg/controller/info/info.go
+++ pkg/controller/info/info.go
@@ -50,12 +50,28 @@ func maskUser(s, username string) string {
 	return strings.ReplaceAll(s, username, "(USER)")
 }
 
-func (c *Controller) Info(_ context.Context, _ *logrus.Entry, param *config.Param) error { //nolint:funlen
+func getCurrentUserName() (string, error) {
 	currentUser, err := user.Current()
 	if err != nil {
-		return fmt.Errorf("get a current user: %w", err)
+		return "", fmt.Errorf("get a current user: %w", err)
 	}
 	userName := currentUser.Username
+	if runtime.GOOS == "windows" {
+		// On Windows, the user name is in the form of "domain\user".
+		domain, userName, ok := strings.Cut(userName, "\\")
+		if ok {
+			return userName, nil
+		}
+		return domain, nil
+	}
+	return userName, nil
+}
+
+func (c *Controller) Info(_ context.Context, _ *logrus.Entry, param *config.Param) error { //nolint:funlen
+	userName, err := getCurrentUserName()
+	if err != nil {
+		return fmt.Errorf("get a current user name: %w", err)
+	}
 
 	filePaths := c.finder.Finds(param.PWD, param.ConfigFilePath)
 	cfgs := make([]*Config, len(filePaths))
