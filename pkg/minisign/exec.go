a845aca8c88ef7e85c47597b0b826196d5f986f7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-07-12 10:27:44 +0900

    fix: fix minisign exe path

--- pkg/minisign/exec.go
+++ pkg/minisign/exec.go
@@ -30,6 +30,7 @@ type ExecutorImpl struct {
 func NewExecutor(executor CommandExecutor, param *config.Param) (*ExecutorImpl, error) {
 	rt := runtime.NewR()
 	pkg := Package()
+	pkg.PackageInfo.OverrideByRuntime(rt)
 	exePath, err := pkg.ExePath(param.RootDir, pkg.PackageInfo.Files[0], rt)
 	if err != nil {
 		return nil, fmt.Errorf("get an executable file path of minisign: %w", err)
