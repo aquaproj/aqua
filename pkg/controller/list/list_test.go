e35c801d27d1b480a1ace0a03d4bd0047b10df7c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-09-22 14:26:43 +0900

    refactor: sort logE

--- pkg/controller/list/list_test.go
+++ pkg/controller/list/list_test.go
@@ -63,7 +63,7 @@ packages:
 				t.Fatal(err)
 			}
 			ctrl := list.NewController(finder.NewConfigFinder(fs), reader.New(fs, d.param), registry.New(d.param, downloader, fs, rt, &cosign.MockVerifier{}, &slsa.MockVerifier{}), fs)
-			if err := ctrl.List(ctx, d.param, logE); err != nil {
+			if err := ctrl.List(ctx, logE, d.param); err != nil {
 				if d.isErr {
 					return
 				}
