d4590702c8eeb66396123433f5d0c1473bde1172
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-10-12 18:41:28 +0900

    refactor: fix a lint error

--- pkg/controller/settoken/set.go
+++ pkg/controller/settoken/set.go
@@ -12,7 +12,7 @@ import (
 )
 
 func (c *Controller) Set(ctx context.Context, logE *logrus.Entry) error {
-	fmt.Print("Enter a GitHub acccess token for aqua: ")
+	fmt.Fprint(c.stdout, "Enter a GitHub acccess token for aqua: ")
 	text, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		return fmt.Errorf("read a GitHub Access Token from stdin: %w", err)
