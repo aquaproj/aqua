f248882e515e58f3b72d0621034edf82ee070207
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-11-19 22:38:36 +0900

    fix: sort versions

--- pkg/versiongetter/go.go
+++ pkg/versiongetter/go.go
@@ -3,6 +3,7 @@ package versiongetter
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/aquaproj/aqua/v2/pkg/config/registry"
 	"github.com/aquaproj/aqua/v2/pkg/fuzzyfinder"
@@ -44,14 +45,24 @@ func (g *GoGetter) Get(ctx context.Context, logE *logrus.Entry, pkg *registry.Pa
 }
 
 func (g *GoGetter) List(ctx context.Context, logE *logrus.Entry, pkg *registry.PackageInfo, _ []*Filter, _ int) ([]*fuzzyfinder.Item, error) {
-	versions, err := g.gc.List(ctx, pkg.GoVersionPath)
+	vs, err := g.gc.List(ctx, pkg.GoVersionPath)
 	if err != nil {
 		return nil, fmt.Errorf("list versions: %w", err)
 	}
+	versions := make(version.Collection, 0, len(vs))
+	for _, v := range vs {
+		v, err := version.NewSemver(v)
+		if err != nil {
+			logE.WithError(err).WithField("version", vs).Warn("parse a version")
+			continue
+		}
+		versions = append(versions, v)
+	}
+	sort.Sort(sort.Reverse(versions))
 	items := make([]*fuzzyfinder.Item, len(versions))
 	for i, version := range versions {
 		items[i] = &fuzzyfinder.Item{
-			Item: version,
+			Item: version.Original(),
 		}
 	}
 	return items, nil
