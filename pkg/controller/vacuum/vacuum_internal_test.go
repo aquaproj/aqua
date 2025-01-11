fc4ead0361ecab51a18e02c9b43d70e9d8131f23
Nikita COEUR <nikita.coeur@allopneus.com>
2025-01-11 09:27:42 +0100

    chore(vacuum): fix lint tests

--- pkg/controller/vacuum/vacuum_internal_test.go
+++ pkg/controller/vacuum/vacuum_internal_test.go
@@ -7,9 +7,11 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHandleAsyncStorePackage_NilPackage(t *testing.T) {
+	t.Parallel()
 	logE := logrus.NewEntry(logrus.New())
 
 	vacuumCtrl := New(nil, nil)
@@ -18,44 +20,48 @@ func TestHandleAsyncStorePackage_NilPackage(t *testing.T) {
 	err := vacuumCtrl.handleAsyncStorePackage(logE, nil)
 
 	// Assert
-	assert.Error(t, err)
+	require.Error(t, err)
 	assert.Equal(t, "vacuumPkg is nil", err.Error())
 }
+
 func TestEncodePackageEntry(t *testing.T) {
+	t.Parallel()
 	pkgEntry := &PackageEntry{
 		LastUsageTime: time.Now(),
 		Package:       &Package{Name: "test-package"},
 	}
 
 	data, err := encodePackageEntry(pkgEntry)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.NotNil(t, data)
 
 	var decodedEntry PackageEntry
 	err = json.Unmarshal(data, &decodedEntry)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, pkgEntry.LastUsageTime.Unix(), decodedEntry.LastUsageTime.Unix())
 	assert.Equal(t, pkgEntry.Package.Name, decodedEntry.Package.Name)
 }
 
 func TestDecodePackageEntry(t *testing.T) {
+	t.Parallel()
 	pkgEntry := &PackageEntry{
 		LastUsageTime: time.Now(),
 		Package:       &Package{Name: "test-package"},
 	}
 
 	data, err := json.Marshal(pkgEntry)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	decodedEntry, err := decodePackageEntry(data)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.NotNil(t, decodedEntry)
 	assert.Equal(t, pkgEntry.LastUsageTime.Unix(), decodedEntry.LastUsageTime.Unix())
 	assert.Equal(t, pkgEntry.Package.Name, decodedEntry.Package.Name)
 }
 
 func TestDecodePackageEntry_Error(t *testing.T) {
+	t.Parallel()
 	_, err := decodePackageEntry([]byte("invalid json"))
-	assert.Error(t, err)
+	require.Error(t, err)
 	assert.Contains(t, err.Error(), "unmarshal package entry")
 }
