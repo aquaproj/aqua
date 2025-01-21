0dadc3032bd304069932f8c2ffd8143b92460118
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-01-21 21:57:39 +0900

    refactor: remove an unused field

--- pkg/controller/vacuum/controller.go
+++ pkg/controller/vacuum/controller.go
@@ -2,27 +2,23 @@ package vacuum
 
 import (
 	"context"
-	"io"
-	"os"
 
 	"github.com/aquaproj/aqua/v2/pkg/config"
 	"github.com/spf13/afero"
 )
 
 type Controller struct {
-	stdout io.Writer
-	Param  *config.Param
-	fs     afero.Fs
-	db     *DB
+	Param *config.Param
+	fs    afero.Fs
+	db    *DB
 }
 
 // New initializes a Controller with the given context, parameters, and dependencies.
 func New(ctx context.Context, param *config.Param, fs afero.Fs) *Controller {
 	vc := &Controller{
-		stdout: os.Stdout,
-		Param:  param,
-		fs:     fs,
-		db:     NewDB(ctx, param, fs),
+		Param: param,
+		fs:    fs,
+		db:    NewDB(ctx, param, fs),
 	}
 	return vc
 }
