41ed0c428b908ce71b68c8fb34629f4f779b8931
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-02-20 14:36:44 +0900

    refactor: fix lint errors

--- pkg/minisign/version.go
+++ pkg/minisign/version.go
@@ -6,12 +6,14 @@ import (
 	"github.com/aquaproj/aqua/v2/pkg/checksum"
 )
 
-var Version string //nolint:gochecknoglobals
-//go:embed aqua.yaml
-var aquaBytes []byte //nolint:gochecknoglobals
-//go:embed aqua-checksums.json
-var checksumBytes []byte       //nolint:gochecknoglobals
-var checksums = checksum.New() //nolint:gochecknoglobals
+var (
+	//go:embed aqua.yaml
+	aquaBytes []byte
+	//go:embed aqua-checksums.json
+	checksumBytes []byte
+	Version       string           //nolint:gochecknoglobals
+	checksums     = checksum.New() //nolint:gochecknoglobals
+)
 
 func init() { //nolint:gochecknoinits
 	Version = checksum.ReadEmbeddedTool(checksums, aquaBytes, checksumBytes)
