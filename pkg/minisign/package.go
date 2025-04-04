6bcf570f36fe065f2be6c77f11dabd0cc26b906a
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-04 19:39:34 +0900

    fix: fix a panic on the platform where minisign isn't supported (#3721)
    * fix: fix a panic on the platform where minisign isn't supported
    
    * ci: test on linux/arm64

--- pkg/minisign/package.go
+++ pkg/minisign/package.go
@@ -4,97 +4,100 @@ import (
 	"github.com/aquaproj/aqua/v2/pkg/config"
 	"github.com/aquaproj/aqua/v2/pkg/config/aqua"
 	"github.com/aquaproj/aqua/v2/pkg/config/registry"
-	"github.com/aquaproj/aqua/v2/pkg/ptr"
 )
 
 func Package() *config.Package { //nolint:funlen
-	return &config.Package{
-		Package: &aqua.Package{
-			Name:    "jedisct1/minisign",
-			Version: Version,
-		},
-		PackageInfo: &registry.PackageInfo{
-			Type:               "github_release",
-			RepoOwner:          "jedisct1",
-			RepoName:           "minisign",
-			VersionConstraints: `false`,
-			VersionOverrides: []*registry.VersionOverride{
-				{
-					VersionConstraints:  `Version == "0.11"`,
-					Asset:               "minisign-{{.Version}}-{{.OS}}.{{.Format}}",
-					Format:              "zip",
-					Rosetta2:            ptr.Bool(true),
-					WindowsARMEmulation: ptr.Bool(true),
-					Replacements: map[string]string{
-						"darwin":  "macos",
-						"windows": "win64",
-						"amd64":   "x86_64",
-						"arm64":   "aarch64",
-					},
-					Overrides: []*registry.Override{
-						{
-							GOOS:   "linux",
-							Format: "tar.gz",
-							Files: []*registry.File{
-								{
-									Name: "minisign",
-									Src:  "minisign-{{.OS}}/{{.Arch}}/minisign",
-								},
-							},
-						},
-						{
-							GOOS: "windows",
-							Files: []*registry.File{
-								{
-									Name: "minisign",
-									Src:  "minisign-win64/minisign.exe",
-								},
+	pkg := &aqua.Package{
+		Name:    "jedisct1/minisign",
+		Version: Version,
+	}
+	if Version == "0.11" {
+		return &config.Package{
+			Package: pkg,
+			PackageInfo: &registry.PackageInfo{
+				Type:                "github_release",
+				RepoOwner:           "jedisct1",
+				RepoName:            "minisign",
+				VersionConstraints:  `false`,
+				Asset:               "minisign-{{.Version}}-{{.OS}}.{{.Format}}",
+				Format:              "zip",
+				Rosetta2:            true,
+				WindowsARMEmulation: true,
+				Replacements: map[string]string{
+					"darwin":  "macos",
+					"windows": "win64",
+					"amd64":   "x86_64",
+					"arm64":   "aarch64",
+				},
+				Overrides: []*registry.Override{
+					{
+						GOOS:   "linux",
+						Format: "tar.gz",
+						Files: []*registry.File{
+							{
+								Name: "minisign",
+								Src:  "minisign-{{.OS}}/{{.Arch}}/minisign",
 							},
 						},
 					},
-					SupportedEnvs: []string{
-						"darwin",
-						"windows",
-						"amd64",
+					{
+						GOOS: "windows",
+						Files: []*registry.File{
+							{
+								Name: "minisign",
+								Src:  "minisign-win64/minisign.exe",
+							},
+						},
 					},
 				},
+				SupportedEnvs: []string{
+					"darwin",
+					"windows",
+					"amd64",
+				},
+			},
+		}
+	}
+	return &config.Package{
+		Package: pkg,
+		PackageInfo: &registry.PackageInfo{
+			Type:                "github_release",
+			RepoOwner:           "jedisct1",
+			RepoName:            "minisign",
+			VersionConstraints:  `false`,
+			Asset:               "minisign-{{.Version}}-{{.OS}}.{{.Format}}",
+			Format:              "zip",
+			WindowsARMEmulation: true,
+			Files: []*registry.File{
 				{
-					VersionConstraints:  `true`,
-					Asset:               "minisign-{{.Version}}-{{.OS}}.{{.Format}}",
-					Format:              "zip",
-					WindowsARMEmulation: ptr.Bool(true),
+					Name: "minisign",
+					Src:  "minisign-{{.OS}}/{{.Arch}}/minisign",
+				},
+			},
+			Replacements: map[string]string{
+				"darwin":  "macos",
+				"windows": "win64",
+				"amd64":   "x86_64",
+				"arm64":   "aarch64",
+			},
+			Overrides: []*registry.Override{
+				{
+					GOOS:   "linux",
+					Format: "tar.gz",
+				},
+				{
+					GOOS: "darwin",
 					Files: []*registry.File{
 						{
 							Name: "minisign",
-							Src:  "minisign-{{.OS}}/{{.Arch}}/minisign",
 						},
-					},
-					Replacements: map[string]string{
-						"darwin":  "macos",
-						"windows": "win64",
-						"amd64":   "x86_64",
-						"arm64":   "aarch64",
-					},
-					Overrides: []*registry.Override{
-						{
-							GOOS:   "linux",
-							Format: "tar.gz",
-						},
-						{
-							GOOS: "darwin",
-							Files: []*registry.File{
-								{
-									Name: "minisign",
-								},
-							},
-						},
-					},
-					SupportedEnvs: []string{
-						"darwin/arm64",
-						"windows",
-						"linux/amd64",
 					},
 				},
+			},
+			SupportedEnvs: []string{
+				"darwin/arm64",
+				"windows",
+				"linux/amd64",
 			},
 		},
 	}
