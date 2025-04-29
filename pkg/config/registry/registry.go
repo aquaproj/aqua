16958411eef24d25bb699c2ade0a58b95cc0590c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-29 10:00:51 +0900

    perf: cache a map of packages

--- pkg/config/registry/registry.go
+++ pkg/config/registry/registry.go
@@ -1,5 +1,17 @@
 package registry
 
+import "github.com/sirupsen/logrus"
+
 type Config struct {
 	PackageInfos PackageInfos `yaml:"packages" json:"packages"`
+	m            map[string]*PackageInfo
+}
+
+func (c *Config) Packages(logE *logrus.Entry) map[string]*PackageInfo {
+	if c.m != nil {
+		return c.m
+	}
+	m := c.PackageInfos.ToMap(logE)
+	c.m = m
+	return m
 }
