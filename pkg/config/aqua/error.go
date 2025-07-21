3f7b9ed8e4ada09d637b504769d775f86923486c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-07-21 16:49:52 +0900

    fix: forbid main and master for ref

--- pkg/config/aqua/error.go
+++ pkg/config/aqua/error.go
@@ -3,9 +3,10 @@ package aqua
 import "errors"
 
 var (
-	errInvalidRegistryType = errors.New("registry type is invalid")
-	errPathIsRequired      = errors.New("path is required for local registry")
-	errRepoOwnerIsRequired = errors.New("repo_owner is required")
-	errRepoNameIsRequired  = errors.New("repo_name is required")
-	errRefIsRequired       = errors.New("ref is required for github_content registry")
+	errInvalidRegistryType     = errors.New("registry type is invalid")
+	errPathIsRequired          = errors.New("path is required for local registry")
+	errRepoOwnerIsRequired     = errors.New("repo_owner is required")
+	errRepoNameIsRequired      = errors.New("repo_name is required")
+	errRefIsRequired           = errors.New("ref is required for github_content registry")
+	errRefCannotBeMainOrMaster = errors.New("ref cannot be 'main' or 'master' for github_content registry")
 )
