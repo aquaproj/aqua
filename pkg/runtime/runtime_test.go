d1f8ea45c17a7be8724819f9b80b882525c8fda3
Shion Ichikawa <shion1305@gmail.com>
2025-12-01 22:48:06 +0900

    🚨 suppress lint errors on nil pointer (SA5011)

--- pkg/runtime/runtime_test.go
+++ pkg/runtime/runtime_test.go
@@ -9,13 +9,13 @@ import (
 func TestNew(t *testing.T) {
 	t.Parallel()
 	rt := runtime.New()
-	if rt == nil {
+	if rt == nil { //nolint:staticcheck
 		t.Fatal("runtime must not be nil")
 	}
-	if rt.GOOS == "" {
+	if rt.GOOS == "" { //nolint:staticcheck
 		t.Fatal("rt.GOOS is empty")
 	}
-	if rt.GOARCH == "" {
+	if rt.GOARCH == "" { //nolint:staticcheck
 		t.Fatal("rt.GOARCH is empty")
 	}
 }
