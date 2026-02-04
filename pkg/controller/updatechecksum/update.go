df1becd78cd5b5990e41676ad61e1eceeb351a1d
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-02-05 06:39:48 +0900

    Refactor Checksum Verification (#4527)
    * refactor: simplify code
    
    * refactor: refactor
    
    * refactor: refactor
    
    * refactor: split functions
    
    * refactor: split functions
    
    * refactor: fix lint errors
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- pkg/controller/updatechecksum/update.go
+++ pkg/controller/updatechecksum/update.go
@@ -24,10 +24,10 @@ func (c *Controller) UpdateChecksum(ctx context.Context, logger *slog.Logger, pa
 		}
 	}
 
-	return c.updateChecksumAll(ctx, logger, param)
+	return c.updateGlobalChecksumFiles(ctx, logger, param)
 }
 
-func (c *Controller) updateChecksumAll(ctx context.Context, logger *slog.Logger, param *config.Param) error {
+func (c *Controller) updateGlobalChecksumFiles(ctx context.Context, logger *slog.Logger, param *config.Param) error {
 	if !param.All {
 		return nil
 	}
@@ -139,13 +139,6 @@ func (c *Controller) updateRegistry(ctx context.Context, logger *slog.Logger, ch
 }
 
 func (c *Controller) updatePackage(ctx context.Context, logger *slog.Logger, checksums *checksum.Checksums, pkg *config.Package, supportedEnvs []string) error {
-	if err := c.getChecksums(ctx, logger, checksums, pkg, supportedEnvs); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Controller) getChecksums(ctx context.Context, logger *slog.Logger, checksums *checksum.Checksums, pkg *config.Package, supportedEnvs []string) error {
 	logger.Info("updating a package checksum")
 	rts, err := checksum.GetRuntimesFromSupportedEnvs(supportedEnvs, pkg.PackageInfo.SupportedEnvs)
 	if err != nil {
@@ -166,7 +159,7 @@ func (c *Controller) getChecksums(ctx context.Context, logger *slog.Logger, chec
 		if !ok {
 			continue
 		}
-		if err := c.getChecksum(ctx, logger, checksums, pkg, checksumFiles, rt, assetNames); err != nil {
+		if err := c.updatePackageByRuntime(ctx, logger, checksums, pkg, checksumFiles, rt, assetNames); err != nil {
 			return err
 		}
 	}
@@ -200,153 +193,124 @@ func (c *Controller) getPkgs(pkg *config.Package, rts []*runtime.Runtime) (map[s
 	return pkgs, assets, nil
 }
 
-func (c *Controller) getChecksum(ctx context.Context, logger *slog.Logger, checksums *checksum.Checksums, pkg *config.Package, checksumFiles map[string]struct{}, rt *runtime.Runtime, assetNames map[string]struct{}) error { //nolint:funlen,cyclop
-	pkgInfo := pkg.PackageInfo
-
+func (c *Controller) getChecksums(ctx context.Context, logger *slog.Logger, pkg *config.Package, checksumFiles map[string]struct{}, rt *runtime.Runtime, assetNames map[string]struct{}, checksumID string) ([]*checksum.Checksum, error) {
 	if !pkg.PackageInfo.Checksum.GetEnabled() {
-		if err := c.dlAssetAndGetChecksum(ctx, logger, checksums, pkg, rt); err != nil {
-			return err
+		cs, err := c.dlAssetAndGetChecksum(ctx, logger, pkg, rt)
+		if err != nil {
+			return nil, err
 		}
-		return nil
-	}
-
-	checksumID, err := pkg.ChecksumID(rt)
-	if err != nil {
-		return fmt.Errorf("get a checksum id: %w", err)
-	}
-
-	if a := checksums.Get(checksumID); a != nil {
-		return nil
+		return []*checksum.Checksum{cs}, nil
 	}
 
 	checksumFileID, err := pkg.RenderChecksumFileID(rt)
 	if err != nil {
-		return fmt.Errorf("render a checksum file ID: %w", err)
+		return nil, fmt.Errorf("render a checksum file ID: %w", err)
 	}
 	if _, ok := checksumFiles[checksumFileID]; ok {
-		return nil
+		return nil, nil
 	}
 	checksumFiles[checksumFileID] = struct{}{}
 	logger.Debug("downloading a checksum file")
 	file, _, err := c.chkDL.DownloadChecksum(ctx, logger, rt, pkg)
 	if err != nil {
-		return fmt.Errorf("download a checksum file: %w", err)
+		return nil, fmt.Errorf("download a checksum file: %w", err)
 	}
 	if file == nil {
-		return nil
+		return nil, nil
 	}
 	defer file.Close()
 	b, err := io.ReadAll(file)
 	if err != nil {
-		return fmt.Errorf("read a checksum file: %w", err)
+		return nil, fmt.Errorf("read a checksum file: %w", err)
 	}
-	checksumFile := strings.TrimSpace(string(b))
-	if pkgInfo.Checksum.FileFormat == "raw" {
-		logger.Debug("set a checksum",
-			"checksum_id", checksumID,
-			"checksum", checksumFile)
-		checksums.Set(checksumID, &checksum.Checksum{
-			ID:        checksumID,
-			Checksum:  checksumFile,
-			Algorithm: pkgInfo.Checksum.GetAlgorithm(),
-		})
-		return nil
-	}
+	return c.getChecksumsFromChecksumFile(pkg, assetNames, checksumID, strings.TrimSpace(string(b)))
+}
+
+func (c *Controller) getChecksumsFromChecksumFile(pkg *config.Package, assetNames map[string]struct{}, checksumID string, checksumFile string) ([]*checksum.Checksum, error) {
+	pkgInfo := pkg.PackageInfo
 	m, s, err := checksum.ParseChecksumFile(checksumFile, pkgInfo.Checksum)
 	if err != nil {
-		return fmt.Errorf("parse a checksum file: %w", err)
+		return nil, fmt.Errorf("parse a checksum file: %w", err)
 	}
 	if s != "" {
-		logger.Debug("set a checksum",
-			"checksum_id", checksumID,
-			"checksum", s)
-		checksums.Set(checksumID, &checksum.Checksum{
-			ID:        checksumID,
-			Checksum:  s,
-			Algorithm: pkgInfo.Checksum.GetAlgorithm(),
-		})
-		return nil
+		return []*checksum.Checksum{
+			{
+				ID:        checksumID,
+				Checksum:  s,
+				Algorithm: pkgInfo.Checksum.GetAlgorithm(),
+			},
+		}, nil
 	}
-	if len(m) == 1 {
-		// get the asset name
-		asset, err := pkg.RenderAsset(rt)
-		if err != nil {
-			return fmt.Errorf("render an asset: %w", err)
-		}
-		chksum, ok := m[asset]
-		if !ok {
-			// if the asset name is different, skip
-			return nil
-		}
-		// update the checksum
-		logger.Debug("set a checksum",
-			"checksum_id", checksumID,
-			"checksum", chksum)
-		checksums.Set(checksumID, &checksum.Checksum{
-			ID:        checksumID,
-			Checksum:  chksum,
-			Algorithm: pkgInfo.Checksum.GetAlgorithm(),
-		})
-		return nil
-	}
+	arr := make([]*checksum.Checksum, 0, len(m))
 	for assetName, chksum := range m {
 		if _, ok := assetNames[assetName]; !ok {
 			continue
 		}
 		checksumID, err := pkg.ChecksumIDFromAsset(assetName)
 		if err != nil {
-			return fmt.Errorf("get a checksum id from asset: %w", err)
+			return nil, fmt.Errorf("get a checksum id from asset: %w", err)
 		}
-		logger.Debug("set a checksum",
-			"checksum_id", checksumID,
-			"checksum", chksum)
-		checksums.Set(checksumID, &checksum.Checksum{
+		arr = append(arr, &checksum.Checksum{
 			ID:        checksumID,
 			Checksum:  chksum,
 			Algorithm: pkgInfo.Checksum.GetAlgorithm(),
 		})
 	}
-	return nil
+	return arr, nil
 }
 
-func (c *Controller) dlAssetAndGetChecksum(ctx context.Context, logger *slog.Logger, checksums *checksum.Checksums, pkg *config.Package, rt *runtime.Runtime) (gErr error) {
-	attrs := slogerr.NewAttrs(1)
-	defer func() {
-		if gErr != nil {
-			gErr = attrs.With(gErr)
-		}
-	}()
+func (c *Controller) updatePackageByRuntime(ctx context.Context, logger *slog.Logger, checksums *checksum.Checksums, pkg *config.Package, checksumFiles map[string]struct{}, rt *runtime.Runtime, assetNames map[string]struct{}) error {
 	checksumID, err := pkg.ChecksumID(rt)
 	if err != nil {
 		return fmt.Errorf("get a checksum id: %w", err)
 	}
+
 	if a := checksums.Get(checksumID); a != nil {
 		return nil
 	}
+
+	cs, err := c.getChecksums(ctx, logger, pkg, checksumFiles, rt, assetNames, checksumID)
+	if err != nil {
+		return err
+	}
+	for _, c := range cs {
+		if a := checksums.Get(c.ID); a != nil {
+			continue
+		}
+		checksums.Set(c.ID, c)
+	}
+	return nil
+}
+
+func (c *Controller) dlAssetAndGetChecksum(ctx context.Context, logger *slog.Logger, pkg *config.Package, rt *runtime.Runtime) (*checksum.Checksum, error) {
+	attrs := slogerr.NewAttrs(1)
+	checksumID, err := pkg.ChecksumID(rt)
+	if err != nil {
+		return nil, fmt.Errorf("get a checksum id: %w", err)
+	}
 	assetName, err := pkg.RenderAsset(rt)
 	if err != nil {
-		return fmt.Errorf("get an asset name: %w", err)
+		return nil, fmt.Errorf("get an asset name: %w", err)
 	}
 	logger = attrs.Add(logger, "asset_name", assetName)
 	logger.Info("downloading an asset to calculate the checksum")
 	f, err := download.ConvertPackageToFile(pkg, assetName, rt)
 	if err != nil {
-		return err //nolint:wrapcheck
+		return nil, attrs.With(err) //nolint:wrapcheck
 	}
 	file, _, err := c.downloader.ReadCloser(ctx, logger, f)
 	if err != nil {
-		return fmt.Errorf("download an asset: %w", err)
+		return nil, fmt.Errorf("download an asset: %w", attrs.With(err))
 	}
 	defer file.Close()
 	algorithm := "sha256"
 	chk, err := checksum.CalculateReader(file, algorithm)
 	if err != nil {
-		return fmt.Errorf("calculate an asset: %w", slogerr.With(err, "algorithm", algorithm))
+		return nil, fmt.Errorf("calculate an asset: %w", slogerr.With(attrs.With(err), "algorithm", algorithm))
 	}
-	checksums.Set(checksumID, &checksum.Checksum{
+	return &checksum.Checksum{
 		ID:        checksumID,
 		Checksum:  chk,
 		Algorithm: algorithm,
-	})
-	return nil
+	}, nil
 }
