e05d9952aeb835e42227326d5c9f5ea180a5fc04
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-03-12 08:41:22 +0900

    fix: re-add env field to log (#4629)

--- cmd/aqua/main.go
+++ cmd/aqua/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/aquaproj/aqua/v2/pkg/cli"
+	"github.com/aquaproj/aqua/v2/pkg/runtime"
 	"github.com/suzuki-shunsuke/urfave-cli-v3-util/urfave"
 )
 
 var version = ""
 
 func main() {
-	urfave.Main("aqua", version, cli.Run)
+	urfave.Main("aqua", version, cli.Run, "env", runtime.New().Env())
 }
